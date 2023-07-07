125d5025cc8be64f06f1e62a21007716c09dacef
kimmingye <dusgml3779@gmail.com>
2023-07-07 14:12:05 +0900

    Reverse String by StringBuilder

--- Test/src/Test04.java
+++ Test/src/Test04.java
@@ -1,9 +1,11 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Test04 {
 
 	public static void main(String[] args) {
 
+		Test04 ts = new Test04();
 		Scanner sc = new Scanner(System.in);
 		int count = sc.nextInt();
 		String[] array = new String[count];
@@ -14,38 +16,69 @@ public class Test04 {
 
 		}
 
-		Test04 ts = new Test04();
+		for (String x : ts.solution(count, array)) {
 
-		String[] result = ts.solution(array);
+			System.out.println(x);
 
-		for (String s : result) {
-			System.out.println(s);
 		}
+
+		/*
+		 * Test04 ts = new Test04();
+		 * 
+		 * String[] result = ts.solution(array);
+		 * 
+		 * for (String s : result) { System.out.println(s); }
+		 */
 
 	}
 
-	public String[] solution(String[] array) {
+	/*
+	 * public String[] solution(String[] array) {
+	 * 
+	 * String[] newArr = new String[array.length];
+	 * 
+	 * StringBuffer sb = null;
+	 * 
+	 * for (int i = 0; i < array.length; i++) {
+	 * 
+	 * sb = new StringBuffer();
+	 * 
+	 * String str = array[i];
+	 * 
+	 * for (int j = str.length() - 1; j >= 0; j--) {
+	 * 
+	 * sb.append(str.charAt(j));
+	 * 
+	 * }
+	 * 
+	 * newArr[i] = sb.toString();
+	 * 
+	 * }
+	 * 
+	 * return newArr; }
+	 */
 
-		String[] newArr = new String[array.length];
+	public ArrayList<String> solution(int n, String[] array) {
 
-		StringBuffer sb = null;
+		ArrayList<String> answer = new ArrayList<>();
+		for (String x : array) {
 
-		for (int i = 0; i < array.length; i++) {
+			char[] s = x.toCharArray();
+			int lt = 0, rt = x.length() - 1;
 
-			sb = new StringBuffer();
+			while (lt < rt) {
 
-			String str = array[i];
-
-			for (int j = str.length() - 1; j >= 0; j--) {
-
-				sb.append(str.charAt(j));
+				char tmp = s[lt];
+				s[lt] = s[rt];
+				s[rt] = tmp;
+				lt++;
+				rt--;
 
 			}
-
-			newArr[i] = sb.toString();
+			String tmp = String.valueOf(s);
+			answer.add(tmp);
 
 		}
-
-		return newArr;
+		return answer;
 	}
 }
