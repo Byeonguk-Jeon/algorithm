cfb6b82f8a337d323a77cd9c071e539be978db7b
kimmingye <dusgml3779@gmail.com>
2023-07-31 16:20:19 +0900

    palindrome

--- Test/src/Test06.java
+++ Test/src/Test06.java
@@ -17,9 +17,9 @@ public class Test06 {
 		String answer = "";
 		
 		for(int i =0;i<str.length();i++) {
-			//System.out.println(str.charAt(i)+" "+i+" "+ str.indexOf(str.charAt(i)));
+			
 			if(str.indexOf(str.charAt(i))==i) answer+=str.charAt(i);
-		
+			
 		}
 		
 		return answer;
---
+++ Test/src/Test07.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Test07 {
+
+	public static void main(String[] args) {
+		
+		Test07 t = new Test07();
+
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		System.out.print(t.solution(str));
+
+		sc.close();
+	}
+
+	public boolean solution(String str) {
+
+			for(int i =0;i<str.length()/2;i++) {
+				
+				if(!Character.toString(str.charAt(i)).equals(Character.toString(str.charAt(str.length()-1-i)))) return false;
+				
+			}
+				
+		return true;
+	}	
+}
