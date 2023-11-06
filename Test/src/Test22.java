922501ded538ae699330be837ebfa577d6d66855
kimmingye <dusgml3779@gmail.com>
2023-11-06 22:04:02 +0900

    연속부분수열

--- Test/src/Test22.java
+++ Test/src/Test22.java
@@ -2,15 +2,15 @@
 public class Test22 {
 
 	public static void main(String[] args) {
-		int[] box = { 12, 15, 11, 20, 25, 10, 20, 19, 13, 15 };
+		int[] box = { 12, 115, 11, 22, 25, 10, 20, 19, 113, 15 };
 
-		int max = 38;
+		int max = box[0] + box[1] + box[2];
 		int sum = 0;
 		for (int i = 1; i < box.length - 3; i++) {
 
-			sum = box[i] + box[i+1] + box[i+2];
-			if(max<sum) {
-				max=sum;
+			sum = box[i] + box[i + 1] + box[i + 2];
+			if (max < sum) {
+				max = sum;
 			}
 
 		}
---
+++ Test/src/Test23.java
@@ -0,0 +1,23 @@
+
+public class Test23 {
+
+	public static void main(String[] args) {
+
+		int target = 6;
+		int arr[] = { 1, 2, 1, 3, 1, 1, 1, 2 };
+		int sum = 0;
+		int count = 0, lt = 0;
+		for (int rt = 0; rt < arr.length; rt++) {
+			sum += arr[rt];
+			if (sum == target)
+				count++;
+			while (sum >= target) {
+				sum -= arr[lt++];
+				if (sum == target) {
+					count++;
+				}
+			}
+		}
+		System.out.println(count);
+	}
+}
